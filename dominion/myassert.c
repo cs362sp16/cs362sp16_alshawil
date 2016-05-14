b6342365d85f854a623beed1bb450bbb342a6f65
luay alshawi <you@example.com>
2016-05-13 22:15:38 -0700

    Forgot to commit some files

--- dominion/myassert.c
+++ dominion/myassert.c
@@ -2,9 +2,11 @@ int failed = 0;
 
 int myassert(int b,char* msg) {
   if (b == 0) {
-    printf("FAILED ASSERTION: %s\n",msg);
+    printf("	FAILED ASSERTION: %s\n",msg);
     failed = 1;
+    return failed;
   }
+  return 0;
 }
 
 void checkasserts() {
